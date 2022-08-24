0c4f0b1c056c9fd912e5ebf300f6d680eb417c5c
ZonghengKaren <31526975+ZonghengKaren@users.noreply.github.com>
2022-08-25 00:13:07 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -23,7 +23,7 @@ module.exports = {
   productionSourceMap: false,
   devServer: {
     port: port,
-    open: false, // 启动项目以后自动打开浏览器
+    open: true, // 启动项目以后自动打开浏览器
     hot: true, // 模块热替换(HMR - hot module replacement)功能会在应用程序运行过程中，替换、添加或删除 模块，而无需重新加载整个页面。主要是通过以下几种方式，来显著加快开发速度
     hotOnly: false, // hot 和 hotOnly 的区别是在某些模块不支持热更新的情况下，前者会自动刷新页面，后者不会刷新页面，而是在控制台输出热更新失败
     overlay: {
